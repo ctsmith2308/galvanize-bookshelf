3e4f9f608719671539fd5ea6428504a426737d4a
Christopher Smith <ctsmith2308@gmail.com>
2017-09-27 15:41:48 -0600

    get, post, patch complete

--- routes/books.js
+++ routes/books.js
@@ -18,20 +18,123 @@ router.get('/books', function(req, res, next) {
     .catch((err) => next(err))
 });
 
-router.get('/books/:id', function(req, res, next){
+router.get('/books/:id', function(req, res, next) {
   let itemsId = req.params.id
   knex('books')
     .select('author', 'cover_url as coverUrl', 'description', 'genre', 'id', 'title', 'updated_at as updatedAt', 'created_at as createdAt')
-    .orderBy('title','asc')
+    .orderBy('title', 'asc')
     .where('id', itemsId)
     .then((items) => {
-      if(items.length <1){
+      if (items.length < 1) {
         res.sendStatus(404)
       }
       res.setHeader('Content-Type', 'application/json')
       res.send(JSON.stringify(items[0]))
     })
     .catch((err) => next(err))
+})
 
+
+
+router.post('/books', (req, res, next) => {
+  // COMMENT: Makes sure that the information that you want is in the
+  // COMMENT: request body.
+
+  knex('books')
+    .insert({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.coverUrl
+    }, '*')
+    .then((book) => {
+      const newObj = {
+        id: book[0].id,
+        title: book[0].title,
+        author: book[0].author,
+        genre: book[0].genre,
+        description: book[0].description,
+        coverUrl: book[0].cover_url
+      }
+      res.send(newObj);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.patch('/books/:id', function(req, res, next) {
+  const id = req.params.id
+
+  const {
+    description,
+    author,
+    genre,
+    title,
+    coverUrl
+  } = req.body
+
+  let newObj = {}
+
+  if (author) {
+    newObj.author = author
+  }
+  if (genre) {
+    newObj.genre = genre
+  }
+  if (title) {
+    newObj.title = title
+  }
+  if (coverUrl) {
+    newObj.cover_url = coverUrl
+  }
+  if (description) {
+    newObj.description = description
+  }
+  return knex('books')
+    .select()
+    .where('id', id)
+    .then((rowsAffected) => {
+      if (!rowsAffected) {
+        return res.sendStatus(404)
+      }
+      knex('books')
+        .update(newObj)
+        .where('id', id)
+        .returning('*')
+        .then((data) => {
+          let newBook = {
+            id: data[0].id,
+            author: data[0].author,
+            genre: data[0].genre,
+            title: data[0].title,
+            coverUrl: data[0].cover_url,
+            description: data[0].description
+          }
+          res.send(newBook)
+        })
+        .catch((err) => next(err))
+    })
+})
+
+
+router.delete('/books/:id', function(req, res, next) {
+  let delItem = req.params.id
+  knex('books')
+    .del()
+    .where('id', delItem)
+    .then((rowsAffected) => {
+      if (rowsAffected !== 1) {
+        return res.sendStatus(404)
+      }
+      console.log('you deleted', delItem)
+      console.log(rowsAffected)
+      res.setHeader('Content-Type', 'application/json')
+      res.send(JSON.stringify(rowsAffected[0]))
+
+      // res.sendStatus(200)
+    })
+    .catch((err) => next(err))
 })
 module.exports = router;
