2b406a93245e90161448f39c8fabb5faef420a3d
Christopher Smith <ctsmith2308@gmail.com>
2017-09-27 22:35:57 -0600

    migrations table complete minus hashed password type

--- migrations/20170927215441_users.js
+++ migrations/20170927215441_users.js
@@ -1,7 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users',(table){
-
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id')
+    table.string('first_name',255).notNullable().defaultTo('')
+    table.string('last_name',255).notNullable().defaultTo('')
+    table.string('email',255).notNullable().unique()
+    table.specificType('hashed_password','character').notNullable()
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
   })
 };
 
