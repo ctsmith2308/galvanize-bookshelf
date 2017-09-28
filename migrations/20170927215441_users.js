2ff1a947a8cb4db644111de6c04f06212149f4ab
Christopher Smith <ctsmith2308@gmail.com>
2017-09-28 09:21:27 -0600

    seed file complete

--- migrations/20170927215441_users.js
+++ migrations/20170927215441_users.js
@@ -2,10 +2,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', (table) => {
     table.increments('id')
-    table.string('first_name',255).notNullable().defaultTo('')
-    table.string('last_name',255).notNullable().defaultTo('')
-    table.string('email',255).notNullable().unique()
-    table.specificType('hashed_password','char(60)').notNullable()
+    table.string('first_name', 255).notNullable().defaultTo('')
+    table.string('last_name', 255).notNullable().defaultTo('')
+    table.string('email', 255).notNullable().unique()
+    table.specificType('hashed_password', 'char(60)').notNullable()
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
   })
@@ -13,5 +13,4 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('users')
-
 };
